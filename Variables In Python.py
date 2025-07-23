f9b5c3eca645c1d09e1ad938e50a649b1fe865d7
yashupatel1998 <yashasvipatel19@gmail.com>
2025-07-23 14:11:14 -0400

    initial commit

---
+++ Variables In Python.py
@@ -0,0 +1,20 @@
+# To define value stored in a memory location we use varible
+# In this code Name, age and Fav_Color is Varibles which hold respective values
+""" The rules for giving variable names in Python are: 
+Start with a letter or an underscore:
+Variable names must begin with either a letter (a-z, A-Z) or an underscore (_). They cannot start with a number.
+Contain only alphanumeric characters and underscores:
+After the initial letter or underscore, the rest of the variable name can consist of letters, numbers (0-9), and underscores. Special characters like @, #, $, %, etc., are not allowed.
+No spaces:
+Variable names cannot contain spaces. For multi-word variable names, use underscores to separate words (e.g., my_variable, total_price). This is known as "snake_case" and is the recommended convention in Python.
+Case-sensitive:
+Python variable names are case-sensitive. myVariable, myvariable, and MyVariable are all treated as distinct variables.
+Avoid Python keywords:
+Do not use Python keywords (reserved words with special meanings, like if, else, for, while, class, def, etc.) as variable names. Using a keyword will result in a SyntaxError.
+"""
+Name = "Yash"
+age = 26
+Fav_Color = "White"
+print(Name)
+print(age)
+print(Fav_Color)
